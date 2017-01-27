ce02e02fa2675641e55a6bfffc52269a76b53371
Marty Kovach <tyedye105@yahoo.com>
2017-01-27 15:43:16 -0800

    add filtering functionality to hero list component

--- src/app/hero-list/hero-list.component.ts
+++ src/app/hero-list/hero-list.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { HeroService } from '../hero.service';
 
+
 @Component({
   selector: 'hero-list',
   templateUrl: './hero-list.component.html',
@@ -14,13 +15,16 @@ import { HeroService } from '../hero.service';
 export class HeroListComponent implements OnInit {
     heroes: FirebaseListObservable<any[]>;
     currentRoute: string = this.router.url;
-
-
+    filterBySpecialty: string = "all";
 
   constructor(private router: Router, private heroService: HeroService) { }
 
   ngOnInit() {
     this.heroes = this.heroService.getHeroes();
+  }
+
+  onChange(optionFromMenu) {
+    this.filterBySpecialty = optionFromMenu;
   }
   goToDetailPage(clickedHero) {
     this.router.navigate(['heroes', clickedHero.$key]);
--- src/app/specialty.pipe.ts
+++ src/app/specialty.pipe.ts
@@ -1,12 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Hero } from './hero.model';
 
 @Pipe({
-  name: 'specialty'
+  name: 'specialtyFilter',
+  pure: false
 })
+
 export class SpecialtyPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(input: Hero[], specialty) {
+    var output: Hero[] = [];
+    if( specialty === "all"){
+      output = input
+      return output
+    } else {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].specialty === specialty) {
+          output.push(input[i])
+        }
+      }
+    }
+    return output;
   }
-
 }
